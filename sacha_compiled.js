357450f793302c17abc85ceb545a5153ecbb8565
Gabriel Horner <gabriel.horner@gmail.com>
2014-04-20 00:59:22 -0400

    remove generated cruft

---
+++ sacha_compiled.js
@@ -0,0 +1,12 @@
+if(!lt.util.load.provided_QMARK_('lt.plugins.sacha')) {
+goog.provide('lt.plugins.sacha');
+goog.require('cljs.core');
+goog.require('lt.objs.command');
+goog.require('lt.objs.command');
+goog.require('lt.objs.tabs');
+goog.require('lt.objs.tabs');
+goog.require('lt.object');
+goog.require('lt.object');
+}
+
+//# sourceMappingURL=sacha_compiled.js.map
