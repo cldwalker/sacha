f9b42430fd84a5cfc5c96ede5d87beb81c398117
Gabriel Horner <gabriel.horner@gmail.com>
2014-08-03 13:28:30 -0400

    Basic working linked doc for new tab, remove unused existing

--- src/lt/plugins/sacha/util.cljs
+++ src/lt/plugins/sacha/util.cljs
@@ -1,49 +1,60 @@
 (ns lt.plugins.sacha.util
   (:require [lt.objs.document :as doc]
             [lt.objs.editor.pool :as pool]
+            [lt.objs.editor :as editor]
+            [lt.objs.files :as files]
+            [lt.objs.opener :as opener]
+            [lt.objs.metrics :as metrics]
             [lt.object :as object]))
 
+;; TODO: PR to make this available in LT
+(defn open-path
+  "Copy of opener/open-path modified to take existing doc and not create one with doc/open."
+  [obj doc path]
+  (files/open path
+              (fn [data]
+                (let [type (files/path->type path)
+                      ed (pool/create (merge {:doc doc :line-ending (-> @doc :line-ending)} (lt.objs.opener/path->info path)))]
+                  (metrics/capture! :editor.open {:type (or (:name type) (files/ext path))
+                                                  :lines (editor/last-line ed)})
+                  ;; don't want file-backed for zoomed view
+                  ;; (object/add-tags ed [:editor.file-backed])
+                  (object/raise obj :open ed)
+                  (lt.objs.tabs/add! ed)
+                  (lt.objs.tabs/active! ed)))))
+
+;; TODO: PR to make this available in LT
 (defn create-sub
   "Copy of document/create-sub with bug fix"
   ([doc] (create-sub doc nil))
   ([doc info]
-   (let [neue (doc/create (merge (select-keys @doc doc/doc-keys) info {:doc (doc/linked* doc #_(:doc doc) info)
+   (let [neue (doc/create (merge (select-keys @doc doc/doc-keys) info {:doc (doc/linked* doc info)
                                                                :root doc}))]
      (object/add-tags neue [:document.linked])
      (object/update! doc [:sub-docs] conj neue))))
 
-(defn update-editor-to-linked-doc!
-  "Updates given editor to use a linked doc"
-  [ed path doc]
-  (let [info (merge {:mime "plaintext" :tags [:editor.plaintext]}
-                    (lt.objs.opener/path->info path))
-        content (:content (lt.objs.files/open-sync path))
-        ;; TODO: remove hardcoded brittle defaults
-        ;; These defaults may not work for others and for specific file types
-        default-tags #{:editor.inline-result :tabset.tab :editor.keys.vim.normal
-                       :editor.file-backed :object :editor.keys.vim :editor.keys.emacs :editor}
-        default-editor-keys #{:info :lt.object/id :lt.object/type :lt.objs.tabs/tabset :doc :tags
-                              :editor.generation :content :triggers :ed :children
-                              :listeners :behaviors :lt.objs.editor.pool/comment-options}
-        outdated-editor-keys (clojure.set/difference (set (keys @ed)) default-editor-keys)]
-    (lt.objs.document/register-doc doc path)
-    (lt.objs.editor/set-doc! ed doc)
-    ;; these should update listeners
-    (lt.object/remove-tags ed (clojure.set/difference (:tags @ed) default-tags))
-    (lt.object/add-tags ed (into default-tags (:tags info [])))
-    (lt.object/merge! ed {:info info})
-    (swap! ed #(apply dissoc % outdated-editor-keys))
-    (when-let [ts (:lt.objs.tabs/tabset @ed)]
-      (lt.object/raise ts :tab.updated))))
+(defn open-linked-doc
+  "Creates a linked doc from the given editor and linked doc (http://codemirror.net/doc/manual.html#linkedDoc)
+  options and opens it in a new tab. Assumes a subview of the original doc is to be created."
+  [ed ldoc-opts]
+  (create-sub (:doc @ed) ldoc-opts)
+  (open-path opener/opener
+             (-> @ed :doc deref :sub-docs last)
+             (let [path (get-in @ed [:info :path])]
+               (str (files/without-ext path) "-zoom-"
+                    (:from ldoc-opts)  "-to-" (:to ldoc-opts)
+                    "." (files/ext path)))))
 
 (comment
   (def path (get-in @ed [:info :path]))
-  (def ed (first (pool/containing-path "whoop")))
+  (def ed (first (pool/containing-path "sacha.cljs")))
 
   (create-sub (:doc @ed) {:from 1 :to 5})
   (def ldoc (-> @ed :doc deref :sub-docs last))
-  (def ed2 (first (pool/containing-path "codemirror.cljs")))
-  (update-editor-to-linked-doc! ed2 path ldoc)
+
+  (open-path opener/opener ldoc path)
+
+  (open-linked-doc ed {:from 0 :to 15})
 
   ;; diagnostics
   (-> @ed :doc deref :sub-docs count)
